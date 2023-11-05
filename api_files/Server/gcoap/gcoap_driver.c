6078cb8d7a9431d88d7add0e224a7c5562d3159c
triess <timriess@outlook.com>
2023-11-05 15:20:34 +0100

    Changes by Dennis: code cleanup

--- api_files/Server/gcoap/gcoap_driver.c
+++ api_files/Server/gcoap/gcoap_driver.c
@@ -2,31 +2,21 @@
 // Created by sommer on 04.11.23.
 //
 
+//#if USE_GCOAP
+
 #include "gcoap_driver.h"
 
 void gcoap_start_server(struct server_driver_t* sd){
     printf("backend call: starting gcoap server"); //TODO
 
-    // from the example in gcoap in RIOT
-    /* CoAP resources. Must be sorted by path (ASCII order). */
-    static const coap_resource_t _resources[] = {
-            { "/cli/stats", COAP_GET | COAP_PUT, _stats_handler, NULL },
-            { "/riot/board", COAP_GET, _riot_board_handler, NULL },
-    };
-
-    static gcoap_listener_t _listener = {
-            &_resources[0],
-            ARRAY_SIZE(_resources),
-            GCOAP_SOCKET_TYPE_UNDEF,
-            _encode_link,
-            NULL,
-            NULL
-    };
-
-
 
 };
 void gcoap_register_resource(struct server_driver_t* sd, methods_selector_t ms,char* path,resource_handler_t rh){
     printf("backend call: register resource gcoap to path: %s",path); //TODO
+};
+
+server_driver_t gcoap_driver = {
+        .start_server = &gcoap_start_server,
+        .register_resource = &gcoap_register_resource
 };
 
